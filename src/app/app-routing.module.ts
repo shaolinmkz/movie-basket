21518c52b1cbc74a6910f194481a01025a50f3ac
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2019-10-18 23:51:48 +0100

    Build out login and registration form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,13 +7,14 @@ import { LoginComponent } from './login/login.component';
 import { HeroComponent } from './hero/hero.component';
 import { RegisterComponent } from './register/register.component';
 import { FavoritesComponent } from './favorites/favorites.component';
+import { AuthGuard } from './auth-guard/auth-guard';
 
 
 const routes: Routes = [
   { path: '', component: HeroComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'favorites', component: FavoritesComponent },
+  { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard] },
   { path: 'single-view/:id', component: SingleViewComponent },
   { path: '**', component: NotFoundComponent },
 ];
