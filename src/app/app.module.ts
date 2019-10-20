2cab43e9123faa00101f0ad7773e08547cb781ba
Nwabuzor C. Obiora <nwabuzor.obiora@gmail.com>
2019-10-20 01:53:43 +0100

    Implement PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from 'src/app/app-routing.module';
 import { AppComponent } from 'src/app/app.component';
@@ -13,24 +14,26 @@ import { RegisterComponent } from 'src/app/register/register.component';
 import { NotFoundComponent } from 'src/app/not-found/not-found.component';
 import { VideoFrameComponent } from 'src/app/video-frame/video-frame.component';
 import { FavoritesComponent } from 'src/app/favorites/favorites.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroComponent,
-    TopNavComponent,
     MovieCardComponent,
+    VideoFrameComponent,
+    TopNavComponent,
     SingleViewComponent,
+    HeroComponent,
     FooterComponent,
     LoginComponent,
     RegisterComponent,
     NotFoundComponent,
-    VideoFrameComponent,
     FavoritesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
